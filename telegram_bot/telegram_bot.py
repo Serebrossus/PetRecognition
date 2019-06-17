27a34a96303a54a7b5b19f7c2b9b438f276a05fd
Serebrossus <pidtchay@gmail.com>
2019-06-17 14:25:27 +0300

    add message handler 'log'

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,5 +1,6 @@
 import telebot as tb
-import log_manager.loger
+import log_manager.loger as lgr
+from time import sleep
 
 BOT_TOKEN = ''
 
@@ -16,11 +17,26 @@ def start_message(message):
 def repeat_all_messages(message):
     bot.reply_to(message, 'I do not know what is it!')
 
+@bot.message_handler(commands=['log'])
+def send_log(message):
+    log = open(lgr.HISTORY_LOG, 'r')
+    bot.send_document(message.chat.id, log)
+    try:
+        file = open(lgr.LAST_CLEANING_HISTORY, 'r')
+    except FileNotFoundError:
+        bot.send_message(message.chat.id, 'last cleaning history not found')
+    else:
+        date_last_cleaning_log = file.read()
+        file.close()
+    
+    bot.send_message(message.chat.id, '''
+    Last cleaning history in {}
+    '''.format(date_last_cleaning_log))
 
 try:
     bot.polling()
 except OSError as e:
     bot.stop_polling()
-    loger.write_error(type(e).__name__)
+    lgr.write_error(type(e).__name__)
     sleep(10)
     bot.polling()
